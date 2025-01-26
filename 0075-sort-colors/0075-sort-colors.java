09ec56517391424ecca564565d326ee5db90fe74
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-01-26 20:51:08 +0530

    Time: 0 ms (100%), Space: 42 MB (46.18%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,25 +1,47 @@
 class Solution {
     public void sortColors(int[] arr) {
-        int cnt0=0;
-        int cnt1=0;
-        int cnt2=0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]==0){
-                cnt0++;
-            }else if(arr[i]==1){
-                cnt1++;
+        // int cnt0=0;
+        // int cnt1=0;
+        // int cnt2=0;
+        // for(int i=0;i<arr.length;i++){
+        //     if(arr[i]==0){
+        //         cnt0++;
+        //     }else if(arr[i]==1){
+        //         cnt1++;
+        //     }else{
+        //         cnt2++;
+        //     }
+        // }
+        // for(int idx=0;idx<cnt0;idx++){
+        //     arr[idx]=0;
+        // }
+        // for(int idx=cnt0;idx<cnt0+cnt1;idx++){
+        //     arr[idx]=1;
+        // }
+        // for(int idx=cnt0+cnt1;idx<cnt0+cnt1+cnt2;idx++){
+        //     arr[idx]=2;
+        // }
+
+
+        int s=0;
+        int e=arr.length-1;
+        int m=0;
+        while(m<=e){
+            if(arr[m]==0){
+                swap(arr,s,m);
+                s++;
+                m++;
+            }else if(arr[m]==1){
+                m++;
             }else{
-                cnt2++;
+                swap(arr,m,e);
+                e--;
             }
         }
-        for(int idx=0;idx<cnt0;idx++){
-            arr[idx]=0;
-        }
-        for(int idx=cnt0;idx<cnt0+cnt1;idx++){
-            arr[idx]=1;
-        }
-        for(int idx=cnt0+cnt1;idx<cnt0+cnt1+cnt2;idx++){
-            arr[idx]=2;
-        }
+    }
+    public static void swap(int[] arr,int low,int high){
+        int temp=arr[low];
+        arr[low]=arr[high];
+        arr[high]=temp;
     }
 }
