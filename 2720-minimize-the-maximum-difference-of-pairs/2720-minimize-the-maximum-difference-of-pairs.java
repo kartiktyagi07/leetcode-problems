0eea15d3c26f290dc5f62b8058c510006e29c0fe
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-01-29 13:00:57 +0530

    Time: 16 ms (95.19%), Space: 56.3 MB (32.3%) - LeetHub

--- 2720-minimize-the-maximum-difference-of-pairs/2720-minimize-the-maximum-difference-of-pairs.java
+++ 2720-minimize-the-maximum-difference-of-pairs/2720-minimize-the-maximum-difference-of-pairs.java
@@ -8,7 +8,7 @@ class Solution {
         int r=nums[nums.length-1]-nums[0];
         while(l<r){
             int mid=l+(r-l)/2;
-            if(helper(nums,mid)>=p){
+            if(helper(nums,mid,p)){
                 r=mid;
             }else{
                 l=mid+1;
@@ -16,16 +16,16 @@ class Solution {
         }
         return l;
     }
-    public static int helper(int[] nums,int x){
-        int i=1;
+    public static boolean helper(int[] nums,int x,int p){
+        int i=0;
         int cnt=0;
-        while(i<nums.length){
-            if((nums[i]-nums[i-1])<=x){
+        while(i<nums.length-1){
+            if((nums[i+1]-nums[i])<=x){
                 i++;
                 cnt++;
             }
             i++;
         }
-        return cnt;
+        return cnt>=p;
     }
 }
