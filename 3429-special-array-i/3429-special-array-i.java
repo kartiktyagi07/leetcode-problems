6a008ab71dedc67962e032e2df5a8b5cb6d69067
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-02-01 21:48:01 +0530

    Time: 0 ms (100%), Space: 43.6 MB (32.6%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.java
+++ 3429-special-array-i/3429-special-array-i.java
@@ -3,21 +3,29 @@ class Solution {
         if(nums.length==1){
             return true;
         }
+        // for(int i=0;i<nums.length-1;i++){
+        //     int first=nums[i];
+        //     int second=nums[i+1];
+        //     if(first%2==0){
+        //         if(second%2!=0){
+        //             continue;
+        //         }else{
+        //             return false;
+        //         }
+        //     }else{
+        //         if(second%2==0){
+        //             continue;
+        //         }else{
+        //             return false;
+        //         }
+        //     }
+        // }
+        // return true;
+
+        // Seconf Method
         for(int i=0;i<nums.length-1;i++){
-            int first=nums[i];
-            int second=nums[i+1];
-            if(first%2==0){
-                if(second%2!=0){
-                    continue;
-                }else{
-                    return false;
-                }
-            }else{
-                if(second%2==0){
-                    continue;
-                }else{
-                    return false;
-                }
+            if(nums[i]%2==nums[i+1]%2){
+                return false;
             }
         }
         return true;
