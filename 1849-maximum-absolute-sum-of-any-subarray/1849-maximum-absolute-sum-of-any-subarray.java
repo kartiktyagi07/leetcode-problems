0fc2ea4db4527541c3974fbef8751270a024e6e6
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-02-26 09:52:43 +0530

    Time: 3 ms (97.88%), Space: 56.5 MB (56.35%) - LeetHub

---
+++ 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int maxAbsoluteSum(int[] nums) {
+        // BRUTE FORCE IT WILL GIVE TLE
+
+        // int ans=Integer.MIN_VALUE;
+        // for(int i=0;i<nums.length;i++){
+        //     int currSum=0;
+        //     for(int j=i;j<nums.length;j++){
+        //         currSum+=nums[j];
+        //         ans=Math.max(ans,Math.abs(currSum));
+        //     }
+        // }
+        // return ans;
+
+
+
+        // OPTIMIZED APPROACH APPROACH 2
+
+        // KADANES FOR MAXIMUM SUBARRAY
+        int maxSum=Integer.MIN_VALUE;
+        int currSum=0;
+        for(int i=0;i<nums.length;i++){
+            currSum+=nums[i];
+            maxSum=Math.max(maxSum,currSum);
+            if(currSum<=0){
+                currSum=0;
+            }
+        }
+
+        // REVERSE KADANES FOR MINIMUM SUBARRAY
+        int minSum=Integer.MAX_VALUE;
+        int currminSum=0;
+        for(int i=0;i<nums.length;i++){
+            currminSum+=nums[i];
+            minSum=Math.min(minSum,currminSum);
+            if(currminSum>=0){
+                currminSum=0;
+            }
+        }
+        return Math.max(Math.abs(maxSum),Math.abs(minSum));
+    }
+}  
