7c80869d6317a019e3eeff61d1b75a62c2ff3c29
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-03-01 22:08:29 +0530

    Time: 1 ms (96.37%), Space: 44 MB (34.48%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
@@ -1,21 +1,34 @@
 class Solution {
     public int[] applyOperations(int[] nums) {
+        // Approach 1 TC:O(N) SC:O(N)
         for(int i=0;i<nums.length-1;i++){
             if(nums[i]==nums[i+1]){
                 nums[i]*=2;
                 nums[i+1]=0;
             }
         }
-        int[] arr=new int[nums.length];
-        int idx=0;
+        // int[] arr=new int[nums.length];
+        // int idx=0;
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]!=0){
+        //         arr[idx++]=nums[i];
+        //     }
+        // }
+        // for(int i=idx;i<arr.length;i++){
+        //     arr[i]=0;
+        // }
+        // return arr;   
+
+        // Approach 2 TC:O(N) SC:O(1) i.e Not creating another array
+        int l=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
-                arr[idx++]=nums[i];
+                int temp=nums[i];
+                nums[i]=nums[l];
+                nums[l]=temp;
+                l++;
             }
-        }
-        for(int i=idx;i<arr.length;i++){
-            arr[i]=0;
-        }
-        return arr;    
+        } 
+        return nums;
     }
 }
