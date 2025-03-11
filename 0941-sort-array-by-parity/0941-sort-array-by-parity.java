eb41519e43af948a40672c979a48a2b73da1603e
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-03-11 22:00:59 +0530

    Time: 0 ms (100%), Space: 45.1 MB (26.74%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.java
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.java
@@ -1,17 +1,41 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        int[] ans=new int[nums.length];
-        int idx=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]%2==0){
-                ans[idx++]=nums[i];
-            }
-        }
-        for(int i=0;i<nums.length;i++){
+        // Two pass approach
+        // int[] ans=new int[nums.length];
+        // int idx=0;
+        // for(int i=0;i<nums.length;i++){
+        // if(nums[i]%2==0){
+        // ans[idx++]=nums[i];
+        // }
+        // }
+        // for(int i=0;i<nums.length;i++){
+        // if(nums[i]%2!=0){
+        // ans[idx++]=nums[i];
+        // }
+        // }
+        // return ans;
+
+        // One pass approach
+        int i = 0;
+        int j = nums.length - 1;
+        while (i < j) {
             if(nums[i]%2!=0){
-                ans[idx++]=nums[i];
+                if(nums[j]%2!=0){
+                    j--;
+                }else{
+                    swap(nums,i,j);
+                    i++;
+                    j--;
+                }
+            }else{
+                i++;
             }
         }
-        return ans;
+        return nums;
+    }
+    public static void swap(int[] nums,int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
     }
 }
