d941ad706706783731c73aaf64f51fce5eb0d2db
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-03-11 16:45:41 +0530

    Time: 2 ms (43.22%), Space: 45.5 MB (30.73%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.java
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.java
@@ -1,22 +1,40 @@
 class Solution {
-  public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
-    final int kMax = 1000;
-    List<int[]> ans = new ArrayList<>();
-    int[] count = new int[kMax + 1];
-    addCount(nums1, count);
-    addCount(nums2, count);
-    for (int i = 1; i <= kMax; ++i)
-      if (count[i] > 0)
-        ans.add(new int[] {i, count[i]});
-
-    return ans.stream().toArray(int[][] ::new);
-  }
-
-  private void addCount(int[][] nums, int[] count) {
-    for (int[] idAndVal : nums) {
-      final int id = idAndVal[0];
-      final int val = idAndVal[1];
-      count[id] += val;
+    public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
+        // max is taken to find the row size of resultant array which is
+        //   maximum id value in both the array
+        int max=0;
+        for(int[] x:nums1){
+            max=Math.max(max,x[0]);
+        }
+        for(int[] x:nums2){
+            max=Math.max(max,x[0]);
+        }
+        // answer array is initialized with pointers
+        int[][] ans=new int[max][2];
+        int i=0;
+        int j=0;
+        int idx=0;
+        while(i<nums1.length && j<nums2.length){
+            if(nums1[i][0]==nums2[j][0]){
+                ans[idx][0]=nums1[i][0];
+                ans[idx][1]=nums1[i][1]+nums2[j][1];
+                i++;
+                j++;
+            }else if(nums1[i][0]<nums2[j][0]){
+                ans[idx]=nums1[i];
+                i++;
+            }else{
+                ans[idx]=nums2[j];
+                j++;
+            }
+            idx++;
+        }
+        while(i<nums1.length){
+            ans[idx++]=nums1[i++];
+        }
+        while(j<nums2.length){
+            ans[idx++]=nums2[j++];
+        }
+        return Arrays.copyOf(ans, idx);
     }
-  }
 }
