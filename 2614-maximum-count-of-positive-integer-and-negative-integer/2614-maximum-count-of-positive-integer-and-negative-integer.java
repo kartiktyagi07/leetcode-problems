5f301c4a107dd1382f64e6ebeaaf5f13d82c004b
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-03-12 11:06:24 +0530

    Time: 0 ms (100%), Space: 44.8 MB (91.27%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.java
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.java
@@ -2,16 +2,53 @@ class Solution {
     public int maximumCount(int[] nums) {
         // Method 1 TC:O(N) SC:O(1)
         // count pos,neg and leave 0
-        int pos=0,neg=0;
-        for(int x:nums){
-            if(x==0){
-                continue;
-            }else if(x>0){
-                pos++;
-            }else{
-                neg++;
+        // int pos=0,neg=0;
+        // for(int x:nums){
+        //     if(x==0){
+        //         continue;
+        //     }else if(x>0){
+        //         pos++;
+        //     }else{
+        //         neg++;
+        //     }
+        // }
+        // return Math.max(pos,neg);
+
+
+
+        // Method 2 TC:O(NLOGN) SC:O(1)
+        int pos=nums.length-ub(nums);
+        int neg=lb(nums);
+        return Math.max(pos,neg);
+    }
+    public static int lb(int[] nums){
+        int s=0;
+        int e=nums.length-1;
+        int idx=nums.length;
+        while(s<=e){
+            int mid=(s+e)/2;
+            if(nums[mid]<0){
+                s=mid+1;
+            }else if(nums[mid]>=0){
+                idx=mid;
+                e=mid-1;
             }
         }
-        return Math.max(pos,neg);
+        return idx;
+    }
+    public static int ub(int[] nums){
+        int s=0;
+        int e=nums.length-1;
+        int idx=nums.length;
+        while(s<=e){
+            int mid=(s+e)/2;
+            if(nums[mid]<=0){
+                s=mid+1;
+            }else if(nums[mid]>0){
+                idx=mid;
+                e=mid-1;
+            }
+        }
+        return idx;
     }
 }
