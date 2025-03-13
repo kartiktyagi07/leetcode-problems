ea7385927944fff4aeb6d5a2737b5b26e9258f30
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-03-13 21:28:41 +0530

    Time: 3 ms (100%), Space: 77.5 MB (36.38%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,25 +1,43 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        // Method 1 
+        // Method 1 TC:O(N) SC:O(N)
         // Creates 2 lists pos,neg and then modify the nums
-        LinkedList<Integer> pos = new LinkedList<>();
-        LinkedList<Integer> neg = new LinkedList<>();
+
+        // LinkedList<Integer> pos = new LinkedList<>();
+        // LinkedList<Integer> neg = new LinkedList<>();
+        // for(int x:nums){
+        //     if(x>0){
+        //         pos.add(x);
+        //     }else{
+        //         neg.add(x);
+        //     }
+        // }
+        // boolean flag=true;
+        // for(int i=0;i<nums.length;i++){
+        //     if(flag){
+        //         nums[i]=pos.poll();
+        //     }else{
+        //         nums[i]=neg.poll();
+        //     }
+        //     flag=!flag;
+        // }
+        // return nums;
+
+
+
+        // Method 2 Two Pointer Approach TC:O(N) SC:O(1)
+        int[] ans=new int[nums.length];
+        int p=0;
+        int n=1;
         for(int x:nums){
             if(x>0){
-                pos.add(x);
+                ans[p]=x;
+                p+=2;
             }else{
-                neg.add(x);
+                ans[n]=x;
+                n+=2;
             }
         }
-        boolean flag=true;
-        for(int i=0;i<nums.length;i++){
-            if(flag){
-                nums[i]=pos.poll();
-            }else{
-                nums[i]=neg.poll();
-            }
-            flag=!flag;
-        }
-        return nums;
+        return ans;
     }
 }
