73b477a97392c98b3544c2e630ab5b9b71f0c8d0
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-03-17 12:26:19 +0530

    Time: 9 ms (22.48%), Space: 44.7 MB (29.76%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean divideArray(int[] nums) {
+        // Method 1 TC:O(N) SC:O(N)
+        // Use a hashmap to store frequency if frequency is not even 
+        // return false else return true
+
         HashMap<Integer,Integer> map=new HashMap<>();
         for(int x:nums){
-            if(map.isEmpty()){
-                map.put(x,1);
-            }else{
-                map.put(x,map.getOrDefault(x,0)+1);
-            }
+            map.put(x,map.getOrDefault(x,0)+1);
         }
         for(Integer value:map.values()){
             if(value%2!=0){
@@ -14,5 +14,21 @@ class Solution {
             }
         }
         return true;
+
+
+        // Method 2
+        // this xor approach fails beacuse [6,12,11,20,17,4] its xor=0 but we can
+        // make valid pairs 
+
+        // int xor=0;
+        // for(int x:nums){
+        //     xor^=x;
+        // }
+        // return xor==0;
+
+
+
+
+
     }
 }
