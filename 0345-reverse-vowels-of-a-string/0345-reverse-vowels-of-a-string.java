88165f5afa411bd3937a0c6743231edb180b21bd
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-03-29 20:56:01 +0530

    Time: 5 ms (38.39%), Space: 44.9 MB (74.15%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,22 +1,48 @@
 class Solution {
     public String reverseVowels(String s) {
-        ArrayList<Character> list=new ArrayList<>(Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
+        // TC:O(N) SC:O(N)
+        // ArrayList<Character> list=new ArrayList<>(Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
+        // int i=0;
+        // int j=s.length()-1;
+        // StringBuilder sb=new StringBuilder(s);
+        // while(i<j){
+        //     if(list.contains(sb.charAt(i)) && list.contains(sb.charAt(j))){
+        //        char temp=sb.charAt(i);
+        //        sb.setCharAt(i,sb.charAt(j));
+        //        sb.setCharAt(j,temp);
+        //        i++;
+        //        j--;
+        //     }else if(!list.contains(sb.charAt(i))){
+        //         i++;
+        //     }else if(!list.contains(sb.charAt(j))){
+        //         j--;
+        //     }
+        // }
+        // return sb.toString();
+
+
+
+
+        Set<Character> set=new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
         int i=0;
         int j=s.length()-1;
-        StringBuilder sb=new StringBuilder(s);
+        char[] arr = s.toCharArray();
         while(i<j){
-            if(list.contains(sb.charAt(i)) && list.contains(sb.charAt(j))){
-               char temp=sb.charAt(i);
-               sb.setCharAt(i,sb.charAt(j));
-               sb.setCharAt(j,temp);
-               i++;
-               j--;
-            }else if(!list.contains(sb.charAt(i))){
+            if(set.contains(arr[i]) && set.contains(arr[j])){
+                swap(arr,i,j);
                 i++;
-            }else if(!list.contains(sb.charAt(j))){
+                j--;
+            }else if(!set.contains(arr[i])){
+                i++;
+            }else{
                 j--;
             }
         }
-        return sb.toString();
+        return new String(arr);
+    }
+    public static void swap(char[] arr,int i,int j){
+        char temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
     }
 }
