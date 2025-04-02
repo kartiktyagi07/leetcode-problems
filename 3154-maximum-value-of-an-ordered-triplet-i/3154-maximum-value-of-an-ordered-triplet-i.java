128cb3543ccd46f34b4950aa89c168b567c770af
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-04-02 16:33:09 +0530

    Time: 1 ms (99.31%), Space: 41.9 MB (72.34%) - LeetHub

--- 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.java
+++ 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.java
@@ -1,14 +1,34 @@
 class Solution {
     public long maximumTripletValue(int[] nums) {
-        long ans=Integer.MIN_VALUE;
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                for(int k=j+1;k<nums.length;k++){
-                    long val=(long)(nums[i] - nums[j]) * nums[k];
-                    System.out.println(val);
-                    ans=Math.max(ans,val);
-                }
+        // O(N^3) BRUTE FORCE
+        // long ans=Integer.MIN_VALUE;
+        // for(int i=0;i<nums.length;i++){
+        //     for(int j=i+1;j<nums.length;j++){
+        //         for(int k=j+1;k<nums.length;k++){
+        //             long val=(long)(nums[i] - nums[j]) * nums[k];
+        //             System.out.println(val);
+        //             ans=Math.max(ans,val);
+        //         }
+        //     }
+        // }
+        // return ans>0?ans:0;
+
+
+
+        // O(N^2) find prefixxmax before i and suffixmax after i 
+        long ans=0;
+        int n=nums.length;
+        for(int i=1;i<n-1;i++){
+            int prefixMax=Integer.MIN_VALUE;
+            for(int j=0;j<i;j++){
+                prefixMax=Math.max(prefixMax,nums[j]);
             }
+            int suffixMax=Integer.MIN_VALUE;
+            for(int k=i+1;k<n;k++){
+                suffixMax=Math.max(suffixMax,nums[k]);
+            }
+            long val=(long)(prefixMax - nums[i]) * suffixMax;
+            ans=Math.max(ans,val);
         }
         return ans>0?ans:0;
     }
