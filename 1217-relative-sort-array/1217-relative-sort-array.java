724150d474210ba661b252d43568ceb48a471c61
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-04-03 22:05:36 +0530

    Time: 0 ms (100%), Space: 42.1 MB (74.38%) - LeetHub

---
+++ 1217-relative-sort-array/1217-relative-sort-array.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int max=Integer.MIN_VALUE;
+        for(int x:arr1){
+            if(x>max){
+                max=x;
+            }
+        }
+        int[] freq=new int[max+1];
+        for(int x:arr1){
+            freq[x]++;
+        }
+        int idx=0;
+        for(int i=0;i<arr2.length;i++){
+            while(freq[arr2[i]]>0){
+                arr1[idx++]=arr2[i];
+                freq[arr2[i]]--;
+            }
+        }
+        for(int i=0;i<=max;i++){
+            while(freq[i]>0){
+                arr1[idx++]=i;
+                freq[i]--;
+            }
+        }
+        return arr1;
+    }
+}
