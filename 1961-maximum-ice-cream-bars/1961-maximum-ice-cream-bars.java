b5a0d443e0eee30d363eef4891b7c462fda282d0
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-04-04 12:51:05 +0530

    Time: 4 ms (97.44%), Space: 61 MB (74.26%) - LeetHub

--- 1961-maximum-ice-cream-bars/1961-maximum-ice-cream-bars.java
+++ 1961-maximum-ice-cream-bars/1961-maximum-ice-cream-bars.java
@@ -1,15 +1,41 @@
 class Solution {
-    public int maxIceCream(int[] costs, int coins) {
-        int n=costs.length;
-        Arrays.sort(costs);
-        int cnt=0;
-        for(int x:costs){
-            if(coins>=x){
-                coins-=x;
-                cnt++;
-            }else{
-                break;
+    public int maxIceCream(int[] arr, int coins) {
+        // Normal method but we have to do it using counting sort
+        // int n=arr.length;
+        // Arrays.sort(arr);
+        // int cnt=0;
+        // for(int x:arr){
+        //     if(coins>=x){
+        //         coins-=x;
+        //         cnt++;
+        //     }else{
+        //         break;
+        //     }
+        // }
+        // return cnt;
+
+
+        // using count sort
+        int n=arr.length;
+        int max=Integer.MIN_VALUE;
+        for(int x:arr){
+            if(x>max){
+                max=x;
             }
+        }
+        int[] freq=new int[max+1];
+        for(int x:arr){
+            freq[x]++;
+        }
+        int cnt=0;
+        for(int i=1;i<max+1;i++){
+            if(freq[i]==0){
+                continue;
+            }
+            int buy=Math.min(freq[i],coins/i);
+            coins-=buy*i;
+            cnt+=buy;
+            if(coins<i) break;
         }
         return cnt;
     }
