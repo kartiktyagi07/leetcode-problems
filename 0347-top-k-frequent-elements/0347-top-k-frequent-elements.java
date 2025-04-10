a109cde1b1a741d8b68e2f7bfe70f7381cb3b169
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-04-10 09:18:50 +0530

    Time: 11 ms (92.18%), Space: 49.6 MB (5.41%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,22 +1,54 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        ArrayList<Integer> list=new ArrayList<>();
+        // Using Map and Queue TC:O(NlogK)
+
+        // ArrayList<Integer> list=new ArrayList<>();
+        // HashMap<Integer,Integer> map=new HashMap<>();
+        // for(int i=0;i<nums.length;i++){
+        //     map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        // }
+        // PriorityQueue<Map.Entry<Integer, Integer>> minHeap = 
+        //     new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
+        // for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+        //     minHeap.offer(entry);
+        //     if(minHeap.size()>k){
+        //         minHeap.poll();
+        //     }
+        // }
+        // int idx=0;
+        // int[] arr=new int[k];
+        // while(minHeap.size()!=0){
+        //     arr[idx++]=minHeap.poll().getKey();
+        // }
+        // return arr;
+
+
+
+
+        // Using Buckrt Sort TC:O(N)
         HashMap<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
-        PriorityQueue<Map.Entry<Integer, Integer>> minHeap = 
-            new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
+        ArrayList<Integer> buckets[]=new ArrayList[nums.length+1];
+        for(int i=0;i<buckets.length;i++){
+            buckets[i]=new ArrayList<>();
+        }
         for(Map.Entry<Integer,Integer> entry:map.entrySet()){
-            minHeap.offer(entry);
-            if(minHeap.size()>k){
-                minHeap.poll();
+            int key=entry.getKey();
+            int val=entry.getValue();
+            buckets[val].add(key);
+        }
+        List<Integer> list=new ArrayList<>();
+        for(int i=buckets.length-1;i>=0 && list.size()<k;i--){
+            for(int num:buckets[i]){
+                list.add(num);
+                if(list.size()==k) break;
             }
         }
-        int idx=0;
         int[] arr=new int[k];
-        while(minHeap.size()!=0){
-            arr[idx++]=minHeap.poll().getKey();
+        for(int i=0;i<k;i++){
+            arr[i]=list.get(i);
         }
         return arr;
     }
