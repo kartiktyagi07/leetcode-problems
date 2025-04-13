2d8aa5bb8dba349cc30746f94d30e5c6f14a6dda
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-04-13 21:55:28 +0530

    Time: 41 ms (41.23%), Space: 61 MB (32.13%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.java
+++ 0164-maximum-gap/0164-maximum-gap.java
@@ -1,6 +1,7 @@
 class Solution {
     public int maximumGap(int[] nums) {
         // TC:O(NLOGN) due to internal sorting SC:O(1)
+
         int ans=0;
         int n=nums.length;
         if(n<2) return 0;
@@ -10,5 +11,20 @@ class Solution {
             ans=Math.max(ans,diff);
         }
         return ans;
+
+
+
+        // Actual Question is to solve it in O(N) so thats why we are using 
+        // bucket sort to achieve O(N)
+        // int n=nums.length;
+        // int ans=0;
+        // if(n<2) return 0;
+        // // Sorting the array in O(n)
+        
+        // for(int i=0;i<n-1;i++){
+        //     int diff=nums[i+1]-nums[i];
+        //     ans=Math.max(ans,diff);
+        // }
+        // return ans;
     }
 }
