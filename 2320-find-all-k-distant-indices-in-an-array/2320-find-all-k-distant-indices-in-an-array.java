ff0d65401f3ac9a9af73c11c3f27e10013e72c4a
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-04-24 22:32:09 +0530

    Time: 4 ms (78.56%), Space: 45.1 MB (34.59%) - LeetHub

--- 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.java
+++ 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.java
@@ -1,31 +1,46 @@
 class Solution {
     public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
         // Brute force TC:O(N^2) SC:O(N)
+
+        // ArrayList<Integer> list=new ArrayList<>();
+        // LinkedList<Integer> temp=new LinkedList<>();
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]==key){
+        //         temp.add(i);
+        //     }
+        // }
+        // while(temp.size()!=0){
+        //     int val=temp.get(0);
+        //     for(int i=0;i<nums.length;i++){
+        //         int diff=Math.abs(i-val);
+        //         if(diff<=k){
+        //             list.add(i);
+        //         }
+        //     }
+        //     temp.remove(0);
+        // }
+        // List<Integer> uniqueList = list.stream().distinct().collect(Collectors.toList());
+        // return uniqueList;
+
+
+        // Two Pointer Approach TC:O(N);
+        int n=nums.length;
         ArrayList<Integer> list=new ArrayList<>();
-        LinkedList<Integer> temp=new LinkedList<>();
-        for(int i=0;i<nums.length;i++){
+        for(int i=0;i<n;i++){
             if(nums[i]==key){
-                temp.add(i);
+                list.add(i);
             }
         }
-        while(temp.size()!=0){
-            int val=temp.get(0);
-            for(int i=0;i<nums.length;i++){
-                int diff=Math.abs(i-val);
-                if(diff<=k){
-                    list.add(i);
-                }
+        ArrayList<Integer> ans=new ArrayList<>();
+        int j=0;
+        for(int i=0;i<n;i++){
+            while(j<list.size() && list.get(j)<i-k){
+                j++;
             }
-            temp.remove(0);
+            if(j<list.size() && Math.abs(list.get(j)-i)<=k){
+                ans.add(i);
+            }
         }
-        List<Integer> uniqueList = list.stream().distinct().collect(Collectors.toList());
-        return uniqueList;
-
-
-
-
-
-        // Method 2 Two Pointer Approach TC:O() SC:O()
-        
+        return ans;
     }
 }
