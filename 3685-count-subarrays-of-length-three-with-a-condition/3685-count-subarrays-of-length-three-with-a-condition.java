13579d8768a84d0bee8fb20abce8867d24493c33
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-04-27 12:03:47 +0530

    Time: 2 ms (17.85%), Space: 44.8 MB (92.91%) - LeetHub

--- 3685-count-subarrays-of-length-three-with-a-condition/3685-count-subarrays-of-length-three-with-a-condition.java
+++ 3685-count-subarrays-of-length-three-with-a-condition/3685-count-subarrays-of-length-three-with-a-condition.java
@@ -1,18 +1,32 @@
 class Solution {
     public int countSubarrays(int[] nums) {
         // Brute Force TC:O(N^3) SC:O(1)
-        int cnt = 0;
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    if (j == i + 1 && k == j + 1) {
-                        double sum = nums[i] + nums[k];
-                        if (sum == (double) nums[j] / 2) {
-                            cnt++;
-                        }
-                    }
-                }
+
+        // int cnt = 0;
+        // int n = nums.length;
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = i + 1; j < n; j++) {
+        //         for (int k = j + 1; k < n; k++) {
+        //             if (j == i + 1 && k == j + 1) {
+        //                 double sum = nums[i] + nums[k];
+        //                 if (sum == (double) nums[j] / 2) {
+        //                     cnt++;
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
+        // return cnt;
+
+        // OPTIMIZED CODE IN ONE LOOP TC:O(N) SC:O(1)
+        int cnt=0;
+        int n=nums.length;
+        for(int i=0;i<n-2;i++){
+            int first=nums[i];
+            int second=nums[i+1];
+            int third=nums[i+2];
+            if((first+third)==(double)second/2){
+                cnt++;
             }
         }
         return cnt;
