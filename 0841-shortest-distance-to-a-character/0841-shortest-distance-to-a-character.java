809873f7503e9805d79ae2fafd6a5d37d6475291
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-04-29 20:43:21 +0530

    Time: 3 ms (42.99%), Space: 42.6 MB (67.18%) - LeetHub

--- 0841-shortest-distance-to-a-character/0841-shortest-distance-to-a-character.java
+++ 0841-shortest-distance-to-a-character/0841-shortest-distance-to-a-character.java
@@ -1,16 +1,38 @@
 class Solution {
     public int[] shortestToChar(String s, char c) {
+        // TC:O(n*m) n->length of string m->no. of occurence of c
+        // SC:O(n+m)
+
+        // int n=s.length();
+        // int[] ans=new int[n];
+        // ArrayList<Integer> list=new ArrayList<>();
+        // for(int i=0;i<n;i++){
+        //     if(s.charAt(i)==c){
+        //         list.add(i);
+        //     }
+        // }
+        // for(int i=0;i<n;i++){
+        //     int min=findMin(list,i);
+        //     ans[i]=min;
+        // }
+        // return ans;
+
+        // TC:O(N+M) SC:O(N+M)
         int n=s.length();
         int[] ans=new int[n];
-        ArrayList<Integer> list=new ArrayList<>();
+        LinkedList<Integer> list=new LinkedList<>();
         for(int i=0;i<n;i++){
             if(s.charAt(i)==c){
                 list.add(i);
             }
         }
+        int j=0;
+        int cnt=0;
         for(int i=0;i<n;i++){
-            int min=findMin(list,i);
-            ans[i]=min;
+            while(j<list.size()-1 && Math.abs(list.get(j+1)-i)<=Math.abs(list.get(j)-i)){
+                j++;
+            }
+            ans[i]=Math.abs(list.get(j)-i);
         }
         return ans;
     }
