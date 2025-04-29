eff8c02bbacf77e6bc694ef74b98b36d1c54a879
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-04-29 11:11:16 +0530

    Time: 4 ms (99.58%), Space: 45.3 MB (79.87%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,14 +1,21 @@
 class Solution {
     public boolean validPalindrome(String s) {
+        // Edge case check
         if(s.length()<=2){
             return true;
         }
+        // Set two pointers to traverse the string
         int i=0;
         int j=s.length()-1;
         while(i<j){
+            // If the character is mismatched to mtlb ek delete krna pdega
+            // and uske baad dobara palindrome check krna hoga
             if(s.charAt(i)!=s.charAt(j)){
+                // there are 2 possibilities 1-> include i and exclude j
+                // 2-> include j and exclude i
                 return check(s,i+1,j) || check(s,i,j-1);
             }else{
+                // agr mismatched nhi hua to move yout pointers
                 i++;
                 j--;
             }
