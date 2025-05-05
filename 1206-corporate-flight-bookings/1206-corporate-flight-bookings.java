6b43edb180cbb636738a082943440a4602fa3363
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-05-05 22:13:45 +0530

    Time: 3 ms (96.81%), Space: 60.3 MB (21.08%) - LeetHub

--- 1206-corporate-flight-bookings/1206-corporate-flight-bookings.java
+++ 1206-corporate-flight-bookings/1206-corporate-flight-bookings.java
@@ -1,15 +1,34 @@
 class Solution {
     public int[] corpFlightBookings(int[][] bookings, int n) {
-        // Brute Forec Approach
+        // Brute Force Approach
         // TC:O(m*k) m->the number of bookings k-> range from start to end
+
+        // int[] ans=new int[n];
+        // for(int i=0;i<bookings.length;i++){
+        //     int start=bookings[i][0];
+        //     int end=bookings[i][1];
+        //     int val=bookings[i][2];
+        //     for(int j=start;j<=end;j++){
+        //         ans[j-1]+=val;
+        //     }
+        // }
+        // return ans;
+
+
+        // Optimized Code using Prefix Sum Approach
+        // TC:O(m+n)
         int[] ans=new int[n];
         for(int i=0;i<bookings.length;i++){
             int start=bookings[i][0];
             int end=bookings[i][1];
             int val=bookings[i][2];
-            for(int j=start;j<=end;j++){
-                ans[j-1]+=val;
+            ans[start-1]+=val;
+            if(end<n){
+                ans[end]-=val;
             }
+        }
+        for(int i=1;i<n;i++){
+            ans[i]+=ans[i-1];
         }
         return ans;
     }
