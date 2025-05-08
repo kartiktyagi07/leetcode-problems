95e42598e1e28ab048964b7cd4a4a55c2e52833c
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-05-08 21:23:50 +0530

    Time: 6 ms (84.53%), Space: 44.9 MB (19.99%) - LeetHub

--- 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.java
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.java
@@ -1,18 +1,30 @@
 class Solution {
     public int minMoves2(int[] nums) {
-        // Brute Force gives TLE
+        // Brute Force 
         // use long instead of int coz cnt can cause integer overflow
-        long ans=Long.MAX_VALUE;
-        Arrays.sort(nums);
+
+        // long ans=Long.MAX_VALUE;
+        // Arrays.sort(nums);
+        // int n=nums.length;
+        // long cnt=0;
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         long diff=Math.abs((long)nums[i]-nums[j]);
+        //         cnt+=diff;
+        //     }
+        //     ans=Math.min(cnt,ans);
+        //     cnt=0;
+        // }
+        // return (int)ans;
+
+
+        // Optimized code
         int n=nums.length;
-        long cnt=0;
+        Arrays.sort(nums);
+        int median=nums[n/2];
+        long ans=0;
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                long diff=Math.abs((long)nums[i]-nums[j]);
-                cnt+=diff;
-            }
-            ans=Math.min(cnt,ans);
-            cnt=0;
+            ans+=Math.abs((long)nums[i]-median);
         }
         return (int)ans;
     }
