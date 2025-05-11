500a909d2c1bdfc530213a9ca8c473162d431c05
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-05-11 12:52:35 +0530

    Time: 0 ms (100%), Space: 42.5 MB (32.37%) - LeetHub

--- 1293-three-consecutive-odds/1293-three-consecutive-odds.java
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.java
@@ -1,17 +1,31 @@
 class Solution {
     public boolean threeConsecutiveOdds(int[] arr) {
         // Brute Force
+
+        // int n=arr.length;
+        // for(int i=0;i<=n-3;i++){
+        //     int cnt=0;
+        //     for(int j=i;j<i+3;j++){
+        //         if(arr[j]%2!=0){
+        //             cnt++;
+        //         }else{
+        //             break;
+        //         }
+        //     }
+        //     if(cnt==3) return true;
+        // }
+        // return false;
+
+
+        // Second way
         int n=arr.length;
         for(int i=0;i<=n-3;i++){
-            int cnt=0;
             for(int j=i;j<i+3;j++){
-                if(arr[j]%2!=0){
-                    cnt++;
-                }else{
+                if(arr[j]%2==0){
                     break;
                 }
+                if(j==i+2) return true;
             }
-            if(cnt==3) return true;
         }
         return false;
     }
