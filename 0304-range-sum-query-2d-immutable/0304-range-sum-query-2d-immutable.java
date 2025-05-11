430ab2da76d696d938457bc11331d1f245db36e0
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-05-11 13:31:41 +0530

    Time: 108 ms (30.13%), Space: 67.7 MB (72.57%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
@@ -1,17 +1,28 @@
 class NumMatrix {
     int[][] mat;
     public NumMatrix(int[][] matrix) {
-        this.mat=matrix;
+        mat=new int[matrix.length+1][matrix[0].length+1];
+        for(int i=1;i<=matrix.length;i++){
+            for(int j=1;j<=matrix[0].length;j++){
+                mat[i][j]=matrix[i-1][j-1]+mat[i-1][j]+mat[i][j-1]-mat[i-1][j-1];
+            }
+        }
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int sum=0;
-        for(int i=row1;i<=row2;i++){
-            for(int j=col1;j<=col2;j++){
-                sum+=mat[i][j];
-            }
-        }
-        return sum;
+        // It is Brute Force Approach
+
+        // int sum=0;
+        // for(int i=row1;i<=row2;i++){
+        //     for(int j=col1;j<=col2;j++){
+        //         sum+=mat[i][j];
+        //     }
+        // }
+        // return sum;
+
+
+        // Optimized Approach using prefix sum array
+        return mat[row2+1][col2+1] - mat[row1][col2+1] -mat[row2+1][col1] + mat[row1][col1]; 
     }
 }
 
