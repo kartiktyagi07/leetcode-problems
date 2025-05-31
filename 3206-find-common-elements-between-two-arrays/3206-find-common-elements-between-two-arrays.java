0ba483b1ad314e18ba54b1fe62c3327c09dc10d4
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-05-31 22:49:40 +0530

    Time: 7 ms (40.13%), Space: 45.6 MB (45.71%) - LeetHub

--- 3206-find-common-elements-between-two-arrays/3206-find-common-elements-between-two-arrays.java
+++ 3206-find-common-elements-between-two-arrays/3206-find-common-elements-between-two-arrays.java
@@ -24,22 +24,50 @@ class Solution {
 
         // Optimized using Hashing coz in hashSet leekup operation is O(1)
         // TC:O(n1 + n2) SC:O(n1 + n2)
-        HashMap<Integer,Integer> map1=new HashMap<>();
-        HashMap<Integer,Integer> map2=new HashMap<>();
-        for(int i=0;i<nums1.length;i++){
-            map1.put(nums1[i],map1.getOrDefault(nums1[i],0)+1);
+
+        // HashMap<Integer,Integer> map1=new HashMap<>();
+        // HashMap<Integer,Integer> map2=new HashMap<>();
+        // for(int i=0;i<nums1.length;i++){
+        //     map1.put(nums1[i],map1.getOrDefault(nums1[i],0)+1);
+        // }
+        // for(int i=0;i<nums2.length;i++){
+        //     map2.put(nums2[i],map2.getOrDefault(nums2[i],0)+1);
+        // }
+        // int cnt1=0;
+        // for(int i:map2.keySet()){
+        //     if(map1.containsKey(i)) cnt1+=map1.get(i);
+        // }
+        // int cnt2=0;
+        // for(int i:map1.keySet()){
+        //     if(map2.containsKey(i)) cnt2+=map2.get(i);
+        // }
+        // return new int[]{cnt1,cnt2};
+
+        // Using HashSet 
+        Set<Integer> set1 = new HashSet<>();
+        for (int num : nums1) {
+            set1.add(num);
         }
-        for(int i=0;i<nums2.length;i++){
-            map2.put(nums2[i],map2.getOrDefault(nums2[i],0)+1);
+
+        Set<Integer> set2 = new HashSet<>();
+        for (int num : nums2) {
+            set2.add(num);
         }
-        int cnt1=0;
-        for(int i:map2.keySet()){
-            if(map1.containsKey(i)) cnt1+=map1.get(i);
+
+        int cnt1 = 0;
+        for (int num : nums1) {
+            if (set2.contains(num)) {
+                cnt1++;
+            }
         }
-        int cnt2=0;
-        for(int i:map1.keySet()){
-            if(map2.containsKey(i)) cnt2+=map2.get(i);
+
+        int cnt2 = 0;
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                cnt2++;
+            }
         }
-        return new int[]{cnt1,cnt2};
+
+        return new int[]{cnt1, cnt2};
     }
 }
