3f85b7cab35a18ad6725940a6433faa754927848
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-05-31 22:46:42 +0530

    Time: 9 ms (10.7%), Space: 45.4 MB (80.94%) - LeetHub

--- 3206-find-common-elements-between-two-arrays/3206-find-common-elements-between-two-arrays.java
+++ 3206-find-common-elements-between-two-arrays/3206-find-common-elements-between-two-arrays.java
@@ -1,20 +1,44 @@
 class Solution {
     public int[] findIntersectionValues(int[] nums1, int[] nums2) {
-        ArrayList<Integer> list1=new ArrayList<>();
-        ArrayList<Integer> list2=new ArrayList<>();
-        for(int i:nums1){
-            list1.add(i);
+        // Brute Force TC:O(n1*n2) coz contains take O(n) for look up SC:O(n1+n2)
+        // nx=numsx.length;
+
+        // ArrayList<Integer> list1=new ArrayList<>();
+        // ArrayList<Integer> list2=new ArrayList<>();
+        // for(int i:nums1){
+        //     list1.add(i);
+        // }
+        // for(int i:nums2){
+        //     list2.add(i);
+        // }
+        // int cnt1=0;
+        // for(int i:list1){
+        //     if(list2.contains(i)) cnt1++;
+        // }
+        // int cnt2=0;
+        // for(int i:list2){
+        //     if(list1.contains(i)) cnt2++;
+        // }
+        // return new int[]{cnt1,cnt2};
+
+
+        // Optimized using Hashing coz in hashSet leekup operation is O(1)
+        // TC:O(n1 + n2) SC:O(n1 + n2)
+        HashMap<Integer,Integer> map1=new HashMap<>();
+        HashMap<Integer,Integer> map2=new HashMap<>();
+        for(int i=0;i<nums1.length;i++){
+            map1.put(nums1[i],map1.getOrDefault(nums1[i],0)+1);
         }
-        for(int i:nums2){
-            list2.add(i);
+        for(int i=0;i<nums2.length;i++){
+            map2.put(nums2[i],map2.getOrDefault(nums2[i],0)+1);
         }
         int cnt1=0;
-        for(int i:list1){
-            if(list2.contains(i)) cnt1++;
+        for(int i:map2.keySet()){
+            if(map1.containsKey(i)) cnt1+=map1.get(i);
         }
         int cnt2=0;
-        for(int i:list2){
-            if(list1.contains(i)) cnt2++;
+        for(int i:map1.keySet()){
+            if(map2.containsKey(i)) cnt2+=map2.get(i);
         }
         return new int[]{cnt1,cnt2};
     }
