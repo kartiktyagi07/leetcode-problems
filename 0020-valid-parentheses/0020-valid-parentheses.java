4d8e0c8a935f175ed62743114e1824a340a82269
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-06-09 21:09:13 +0530

    Time: 2 ms (97.23%), Space: 41.6 MB (91.69%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -3,19 +3,16 @@ class Solution {
         Stack<Character> st=new Stack<>();
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
-            if(ch ==')' || ch=='}' || ch==']'){
-               if(st.isEmpty()){
-                   return false;
-               }
-               char top=st.peek();
-               if (ch == ')' && top == '(' || 
-                    ch == '}' && top == '{' || 
-                    ch == ']' && top == '[') {
+            if(ch=='}' || ch==')' || ch==']'){
+                if(st.isEmpty()) return false;
+                char top=st.peek();
+                if((ch==']' && top=='[') || (ch=='}' && top=='{') ||     
+                    (ch==')' && top=='(' )){
                     st.pop();
-                } else{
-                   return false;
+                }else{
+                    return false;
                 }
-            }else if(ch=='(' || ch == '{' || ch =='['){
+            }else{
                 st.push(ch);
             }
         }
