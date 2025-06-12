8e5390acf86d1454aa9837f0919ecf24c55051bb
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-06-13 00:05:39 +0530

    Time: 8 ms (26.13%), Space: 42.1 MB (89.86%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.java
@@ -1,23 +1,21 @@
 class Solution {
     public String removeOuterParentheses(String s) {
-        // first of all create a stck to track the opening and closing bracket
-        Stack<Character> st= new Stack<>();
-        // build a sb for optimization
+        if(s.isEmpty()){
+            return "";
+        }
+        Stack<Character> st=new Stack<>();
         StringBuilder sb=new StringBuilder();
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='('){
-                // agr phle se hi h koi to mtlb inner parentheses h to sb m add krdo
+            char ch=s.charAt(i);
+            if(ch=='('){
                 if(st.size()>0){
-                    sb.append(s.charAt(i));
+                    sb.append(ch);
                 }
-                // or push bhi krdo
-                st.push(s.charAt(i));
+                st.push(ch);
             }else{
-                // agr if nhi h yo sbse phle pop kro
                 st.pop();
-                // check the condition k ab bhi stack m h ya nhi agr h to append krdo vrna kch nhi 
                 if(st.size()>0){
-                    sb.append(s.charAt(i));
+                    sb.append(ch);
                 }
             }
         }
