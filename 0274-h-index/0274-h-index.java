b75ee488d14a43a001783a5c9602b4d38ce6f9db
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-06-14 00:36:10 +0530

    Time: 1 ms (71.93%), Space: 41.7 MB (41.43%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int i=0;
+        int n=citations.length;
+        int j=n-1;
+        while(i<=j){
+            int mid=(i+j)/2;
+            if(citations[mid]==n-mid){
+                return n-mid;
+            }else if(citations[mid]<n-mid){
+                i=mid+1;
+            }else{
+                j=mid-1;
+            }
+        }
+        return n-i;
+    }
+}
