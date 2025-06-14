f02b7bbcf61028f3bb4ff273d50abdbf79b27042
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-06-15 00:26:33 +0530

    Time: 1 ms (89.2%), Space: 40.5 MB (70.45%) - LeetHub

---
+++ 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minMaxDifference(int num) {
+        String sb1=new String(String.valueOf(num));
+        String sb2=new String(String.valueOf(num));
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<sb1.length();i++){
+            char ch=sb1.charAt(i);
+            if(ch!='9'){
+                sb1=sb1.replace(ch,'9');
+                break;
+            }
+        }
+        for(int i=0;i<sb2.length();i++){
+            char ch=sb2.charAt(i);
+            if(ch!='0'){
+                sb2=sb2.replace(ch,'0');
+                break;
+            }
+        }
+        return Integer.parseInt(sb1)-Integer.parseInt(sb2);
+    }
+}
