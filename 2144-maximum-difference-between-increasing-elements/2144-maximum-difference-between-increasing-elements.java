338a97318805471ad5e0c48dd3bdcdd17d564fef
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-06-18 10:33:26 +0530

    Time: 5 ms (22.66%), Space: 42 MB (53.62%) - LeetHub

--- 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.java
+++ 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.java
@@ -1,5 +1,7 @@
 class Solution {
     public int maximumDifference(int[] nums) {
+        // Brute Force TC:O(N^2)
+        
         int ans=-1;
         for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
