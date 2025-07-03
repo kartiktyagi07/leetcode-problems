0563776c5cf0e6940566d27e417ae4a36a1b83a6
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-07-03 22:29:28 +0530

    Time: 21 ms (20.37%), Space: 58.6 MB (35.1%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -10,15 +10,25 @@ class Solution {
 
 
         // Using Map
-        Map<Integer,Integer> map=new HashMap<>();
-        for(int i:nums){
-            map.put(i,map.getOrDefault(i,0)+1);
-        }
-        for(int value:map.values()){
-            if(value>1){
+
+        // Map<Integer,Integer> map=new HashMap<>();
+        // for(int i:nums){
+        //     map.put(i,map.getOrDefault(i,0)+1);
+        // }
+        // for(int value:map.values()){
+        //     if(value>1){
+        //         return true;
+        //     }
+        // }
+        // return false;
+
+        // Method 3 Using Sorting
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==nums[i-1]){
                 return true;
             }
-        }
+        } 
         return false;
     }
 }
