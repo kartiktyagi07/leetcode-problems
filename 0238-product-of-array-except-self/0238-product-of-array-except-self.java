5f2d83e9628ed79902540f4ceb39d6b704b16700
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-07-04 23:08:23 +0530

    Time: 2 ms (88.56%), Space: 55.5 MB (50.52%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,46 +1,28 @@
 class Solution {
-    public int[] productExceptSelf(int[] nums) {
-
-        // int[] ans=new int[nums.length];
-        // int prod=1;
-        // int temp=1;
-        // for(int i=0;i<nums.length;i++){
-        //    ans[i]=prod;
-        //    prod*=nums[i];
-        // }
-        // for(int i=nums.length-1;i>=0;i--){
-        //    ans[i]*=temp;
-        //    temp*=nums[i];
-        // }
-        // return ans;
-
-
-
-        // Approach 2 prefix Sum like approach
-        // TC:O(N) SC:O(N)
+    public int[] productExceptSelf(int[] nums){
+        // Prefix and suffix type approach
         int n=nums.length;
-        
-        // Calculate left multiplication array 
-        int[] leftMul=new int[n];
+
+        // build a left multiplication array
+        int [] leftMul=new int[n];
         leftMul[0]=nums[0];
         for(int i=1;i<n;i++){
-           leftMul[i]=leftMul[i-1]*nums[i];
+            leftMul[i]=leftMul[i-1]*nums[i];
         }
 
-        // Calculate right multiplication array
-        int[] rightMul=new int[n];
+        // build a right multiplication array
+        int [] rightMul=new int[n];
         rightMul[n-1]=nums[n-1];
         for(int i=n-2;i>=0;i--){
             rightMul[i]=rightMul[i+1]*nums[i];
         }
 
-        // Filling the answer in nums
+        // Building the answer
         for(int i=0;i<n;i++){
             int left=i>0?leftMul[i-1]:1;
             int right=i<n-1?rightMul[i+1]:1;
             nums[i]=left*right;
-        } 
+        }
         return nums;
     }
-    
 }
