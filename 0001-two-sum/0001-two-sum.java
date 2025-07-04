5603dee64ff101270de0c871bc76ea6ec634852b
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-07-04 11:49:20 +0530

    Time: 2 ms (98.98%), Space: 45.1 MB (30.76%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,17 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // Brute Force Approach TC:O(N^2) SC:O(1)
-
-        // for(int i=0;i<nums.length;i++){
-        //     for(int j=i+1;j<nums.length;j++){
-        //         if(nums[i]+nums[j]==target){
-        //             return new int[]{i,j};
-        //         }
-        //     }
-        // }
-        // return new int[]{-1,-1};
-
-        // Method 2 Using Map Approach TC:O() SC:O() 
+        // Use a map,build a map where key id nums[i] and value is i
         Map<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++){
             int req=target-nums[i];
