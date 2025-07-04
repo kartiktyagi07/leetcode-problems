18c8885be44d9f348815e9ffd5536996312e811d
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-07-04 11:28:09 +0530

    Time: 20 ms (11.56%), Space: 44.7 MB (43.08%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,15 +1,30 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        // Brute Force 
-        // Sort both the arrays and compare them
+        // Brute Force
+        
+        // if(s.length()!=t.length()) return false;
+        // char[] chars1=s.toCharArray();
+        // char[] chars2=t.toCharArray();
+        // Arrays.sort(chars1);
+        // Arrays.sort(chars2);
+        // String str1=new String(chars1);
+        // String str2=new String(chars2);
+        // return str1.equals(str2);
 
-        if(s.length()!=t.length()) return false;
-        char[] chars1=s.toCharArray();
-        char[] chars2=t.toCharArray();
-        Arrays.sort(chars1);
-        Arrays.sort(chars2);
-        String str1=new String(chars1);
-        String str2=new String(chars2);
-        return str1.equals(str2);
+        // Using hashMap
+        Map<Character,Integer> map1=new TreeMap<>();
+        Map<Character,Integer> map2=new TreeMap<>();
+        for(char x:s.toCharArray()){
+            map1.put(x,map1.getOrDefault(x,0)+1);
+        }
+        for(char x:t.toCharArray()){
+            map2.put(x,map2.getOrDefault(x,0)+1);
+        }
+        if(map1.equals(map2)){
+            return true;
+        }else{
+            return false;
+        }
+
     }
 }
