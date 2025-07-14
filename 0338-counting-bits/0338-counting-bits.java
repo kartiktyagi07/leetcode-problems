3ae56131e696a87fc7faac09d8806f4b049ed81c
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-07-14 12:09:24 +0530

    Time: 6 ms (21.01%), Space: 47.1 MB (6.82%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,17 +1,17 @@
 class Solution {
     public int[] countBits(int n) {
-        int[] ans=new int[n+1];
+        int ans[]=new int[n+1];
         for(int i=0;i<=n;i++){
-            ans[i]=setBits(i);
+            ans[i]=helper(i);
         }
         return ans;
     }
-    public static int setBits(int n) {
-        if(n==0) return 0;
+    public int helper(int i){
         int cnt=0;
-        while(n>0){
-            int rmsb=n&(-n);
-            n=n-rmsb;
+        if(i==0) return 0;
+        while(i!=0){
+            int rbsm=i&-i;
+            i=i-rbsm;
             cnt++;
         }
         return cnt;
