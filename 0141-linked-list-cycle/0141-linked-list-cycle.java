26693f2eb05e96fb30cc853ca14934aa208053b1
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-07-15 11:51:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,14 +11,12 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode s=head;
-        ListNode f=head;
-        while(f!=null && f.next!=null){
-            s=s.next;
-            f=f.next.next;
-            if(s==f){
-                return true;
-            }
+        ListNode slow=head;
+        ListNode fast=head;
+        while(slow.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast) return true;
         }
         return false;
     }
