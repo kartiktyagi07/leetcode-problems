700c184f4b512882a8dc074672e8911abd03d708
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-07-22 14:26:14 +0530

    Time: 51 ms (54.83%), Space: 61.7 MB (6.26%) - LeetHub

--- 1813-maximum-erasure-value/1813-maximum-erasure-value.java
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.java
@@ -1,23 +1,44 @@
 class Solution {
     public int maximumUniqueSubarray(int[] nums) {
+        // Using HashMap TC:O(N)
+        // int ans=0;
+        // int n=nums.length;
+        // int left=0;
+        // int right=0;
+        // int cs=0;
+        // HashMap<Integer,Integer> map=new HashMap<>();
+        // while(right<n){
+        //     cs+=nums[right];
+        //     while(!map.isEmpty() && map.containsKey(nums[right])){
+        //         if(map.get(nums[left])==1){
+        //             map.remove(nums[left]);
+        //         }else{
+        //             map.put(nums[left],map.get(nums[left])-1);
+        //         }
+        //         cs-=nums[left];
+        //         left++;
+        //     }
+        //     map.put(nums[right],map.getOrDefault(nums[right],0)+1);
+        //     ans=Math.max(ans,cs);
+        //     right++;
+        // }
+        // return ans;
+
+        // Using HashSet
         int ans=0;
         int n=nums.length;
         int left=0;
         int right=0;
         int cs=0;
-        HashMap<Integer,Integer> map=new HashMap<>();
+        HashSet<Integer> set=new HashSet<>();
         while(right<n){
             cs+=nums[right];
-            while(!map.isEmpty() && map.containsKey(nums[right])){
-                if(map.get(nums[left])==1){
-                    map.remove(nums[left]);
-                }else{
-                    map.put(nums[left],map.get(nums[left])-1);
-                }
+            while(!set.isEmpty() && set.contains(nums[right])){
+                set.remove(nums[left]);
                 cs-=nums[left];
                 left++;
             }
-            map.put(nums[right],map.getOrDefault(nums[right],0)+1);
+            set.add(nums[right]);
             ans=Math.max(ans,cs);
             right++;
         }
