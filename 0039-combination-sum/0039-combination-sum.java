6dfd60a7b5990b4ff052e9d08ac1f310ad1f1414
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-08-01 12:54:21 +0530

    Time: 2 ms (82.75%), Space: 44.8 MB (45.52%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -4,18 +4,34 @@ class Solution {
         helper(0,candidates,target,ans,new ArrayList<>());
         return ans;
     }
-    public void helper(int idx,int[] arr,int target,List<List<Integer>> ans, List<Integer> temp){
-        if(idx==arr.length){
-            if(target==0){
-                ans.add(new ArrayList<>(temp));
-            }
+    // General pattern in recursion + backtracking 
+    public void helper(int idx,int[] arr,int target,List<List<Integer>> ans,List<Integer> temp){
+        if(target==0){
+            ans.add(new ArrayList<>(temp));
             return;
         }
-        if(arr[idx]<=target){
-            temp.add(arr[idx]);
-            helper(idx,arr,target-arr[idx],ans,temp);
+        if(target<0) return;
+        for(int i=idx;i<arr.length;i++){
+            temp.add(arr[i]);
+            helper(i,arr,target-arr[i],ans,temp);
             temp.remove(temp.size()-1);
         }
-        helper(idx+1,arr,target,ans,temp);
     }
+
+    // Method 1 using pick and not pick but not acc. to general pattern
+
+    // public void helper(int idx,int[] arr,int target,List<List<Integer>> ans, List<Integer> temp){
+    //     if(idx==arr.length){
+    //         if(target==0){
+    //             ans.add(new ArrayList<>(temp));
+    //         }
+    //         return;
+    //     }
+    //     if(arr[idx]<=target){
+    //         temp.add(arr[idx]);
+    //         helper(idx,arr,target-arr[idx],ans,temp);
+    //         temp.remove(temp.size()-1);
+    //     }
+    //     helper(idx+1,arr,target,ans,temp);
+    // }
 }
