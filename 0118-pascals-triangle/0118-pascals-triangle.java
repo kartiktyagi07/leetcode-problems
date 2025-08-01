492f6faf55a8e89729b257c3fe4d577218c5a6d9
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-08-01 11:52:14 +0530

    Time: 0 ms (100%), Space: 42 MB (59.76%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,26 +1,20 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        // we will generate a final answer list in which we store all the returned row's list from our function
-        List<List<Integer>> finalans=new ArrayList();
+        List<List<Integer>> ans=new ArrayList<>();
         for(int i=1;i<=numRows;i++){
-            finalans.add(generaterow(i));
+            ans.add(helper(i));
         }
-        return finalans;
-        
+        return ans;
     }
-    // here we make a function to calculate an individual row
-    public List<Integer> generaterow(int row){
-        // as first element is 1 so we initialize ans by 1.
-        int ans=1; 
-        // to add an entire row we make a arraylist so we can store the row in the form of list
-        List<Integer> ansrow=new ArrayList<>();
-        // here we add first item that is 1
-        ansrow.add(ans);
-        for(int i=1;i<row;i++){
-            ans= ans*(row-i);
-            ans=ans/i;
-            ansrow.add(ans);
+    public List<Integer> helper(int n){
+        ArrayList<Integer> temp = new ArrayList<>();
+        int x=1;
+        temp.add(x);
+        for(int i=1;i<n;i++){
+            x=x*(n-i);
+            x=x/i;
+            temp.add(x);
         }
-        return ansrow;
+        return temp;
     }
 }
