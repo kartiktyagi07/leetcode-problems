d64a543e6775d8fdcf3f0a88f8c0836c0d9e6d6a
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-08-19 11:43:41 +0530

    Time: 3 ms (94.03%), Space: 61.5 MB (71.81%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.java
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.java
@@ -1,20 +1,37 @@
 class Solution {
     public long zeroFilledSubarray(int[] nums) {
-        int start=0;
-        int end=0;
+        // Method 1 TC:O(N) SC:O(1)
+
+        // int start=0;
+        // int end=0;
+        // long ans=0;
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]==0){
+        //         start=i;
+        //         end=i+1;
+        //         while(end<nums.length && nums[end]==0){
+        //             end++;
+        //         }
+        //         int n=end-start;
+        //         ans += (long) n * (n + 1) / 2;
+        //         i=end-1;
+        //     }
+        // }
+        // return ans;
+        
+
+        // Method 2 TC:O(N) SC:O(1)
         long ans=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==0){
-                start=i;
-                end=i+1;
-                while(end<nums.length && nums[end]==0){
-                    end++;
-                }
-                int n=end-start;
-                ans += (long) n * (n + 1) / 2;
-                i=end-1;
+        long cnt=0;
+        for(int num:nums){
+            if(num==0){
+                cnt++;
+                ans+=cnt;
+            }else{
+                cnt=0;
             }
         }
         return ans;
+
     }
 }
