a22c0580f8518227838d5072fd4a03a02bd69332
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-08-25 21:02:39 +0530

    Time: 0 ms (100%), Space: 41.6 MB (76.2%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -10,22 +10,47 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        // two pass approach : 1.first fine size  2.delete (size-n)th node from start
+
+        // Two pass approach : 1.first fine size  2.delete (size-n)th node from start
         // TC:O(n) SC:O(1)
-        
-        int size=0;
-        ListNode curr=head;
-        while(curr!=null){
-            size++;
-            curr=curr.next;
+
+        // int size=0;
+        // ListNode curr=head;
+        // while(curr!=null){
+        //     size++;
+        //     curr=curr.next;
+        // }
+        // if(size==n) return head.next;
+        // curr=head;
+        // int condition=size-n;
+        // for(int i=1;i<condition;i++){
+        //     curr=curr.next;
+        // }
+        // curr.next=curr.next.next;
+        // return head;
+
+
+
+        // Single pass approach using two pointers
+
+        // here we initialize a dummy node and points dummy next to head then
+        // we initilaise two pointers slow and fast.first we move slow n times so 
+        // that the diff. b/w slow and fast is exactly n then we move both slow and
+        // fast linearly when slow points null then fast is at correct position then
+        // delete the node.
+
+        ListNode dummy=new ListNode(0);
+        dummy.next=head;
+        ListNode slow=dummy;
+        ListNode fast=dummy;
+        for(int i=0;i<=n;i++){
+            slow=slow.next;
         }
-        if(size==n) return head.next;
-        curr=head;
-        int condition=size-n;
-        for(int i=1;i<condition;i++){
-            curr=curr.next;
+        while(slow!=null){
+            slow=slow.next;
+            fast=fast.next;
         }
-        curr.next=curr.next.next;
-        return head;
+        fast.next=fast.next.next;
+        return dummy.next;
     }
 }
