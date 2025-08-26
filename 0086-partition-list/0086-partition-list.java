2901a4e94c4314db18062ec88efd17a174316a0b
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-08-26 23:07:05 +0530

    Time: 0 ms (100%), Space: 42.3 MB (40.44%) - LeetHub

--- 0086-partition-list/0086-partition-list.java
+++ 0086-partition-list/0086-partition-list.java
@@ -10,30 +10,63 @@
  */
 class Solution {
     public ListNode partition(ListNode head, int x) {
-        // Using extra space TC:O(N) SC:O(N)
-        
-        ArrayList<ListNode> small=new ArrayList<>();
-        ArrayList<ListNode> bigOrEqual=new ArrayList<>();
+        // Doing it inbuilt without extra space TC:O(N) SC:O(1)
+        ListNode beforeHead=new ListNode(0);
+        ListNode afterHead=new ListNode(0);
+        ListNode before=beforeHead;
+        ListNode after=afterHead;
         ListNode curr=head;
         while(curr!=null){
             if(curr.val<x){
-                small.add(curr);
+                before.next=curr;
+                before=before.next;
             }else{
-                bigOrEqual.add(curr);
+                after.next=curr;
+                after=after.next;
             }
             curr=curr.next;
         }
-        ListNode temp=new ListNode(0);
-        ListNode dummy=temp;
-        for(ListNode a:small){
-            dummy.next=a;
-            dummy=dummy.next;
-        }
-        for(ListNode a:bigOrEqual){
-            dummy.next=a;
-            dummy=dummy.next;
-        }
-        dummy.next=null;
-        return temp.next;
+        after.next=null;
+        before.next=afterHead.next;
+        return beforeHead.next;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // // Using extra space TC:O(N) SC:O(N)
+
+        // ArrayList<ListNode> small=new ArrayList<>();
+        // ArrayList<ListNode> bigOrEqual=new ArrayList<>();
+        // ListNode curr=head;
+        // while(curr!=null){
+        //     if(curr.val<x){
+        //         small.add(curr);
+        //     }else{
+        //         bigOrEqual.add(curr);
+        //     }
+        //     curr=curr.next;
+        // }
+        // ListNode temp=new ListNode(0);
+        // ListNode dummy=temp;
+        // for(ListNode a:small){
+        //     dummy.next=a;
+        //     dummy=dummy.next;
+        // }
+        // for(ListNode a:bigOrEqual){
+        //     dummy.next=a;
+        //     dummy=dummy.next;
+        // }
+        // dummy.next=null;
+        // return temp.next;
+
     }
 }
