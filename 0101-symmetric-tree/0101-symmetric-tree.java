2ddfb5bfa0cfd5a333d26cb31e3097a4fafdfd20
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-09-17 14:38:29 +0530

    Time: 128 ms (0.82%), Space: 41.8 MB (69.89%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -21,6 +21,7 @@ class Solution {
     public boolean isMirror(TreeNode t1,TreeNode t2){
         if(t1 == null && t2 == null) return true;
         if(t1 == null || t2 == null) return false;
-        return (t1.val == t2.val) && isMirror(t1.left,t2.right) && isMirror(t1.right,t2.left);
+        if(t1.val != t2.val) return false;
+        return isMirror(t1.left,t2.right) && isMirror(t1.right,t2.left);
     }
 }
