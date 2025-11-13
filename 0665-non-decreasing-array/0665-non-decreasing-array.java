5507d54d467ca833b84fbfe235e3258e9efc9c0a
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-11-13 22:38:29 +0530

    Time: 1 ms (28.66%), Space: 47 MB (12.9%) - LeetHub

--- 0665-non-decreasing-array/0665-non-decreasing-array.java
+++ 0665-non-decreasing-array/0665-non-decreasing-array.java
@@ -1,21 +1,18 @@
 class Solution {
     public boolean checkPossibility(int[] nums) {
-        // check for direct violation if cnt>1 return false
-        // edge case id cnt==1 then we can replace nums[i] or nums[i+1]
         int cnt = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] > nums[i + 1]) {
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]>nums[i+1]){
                 cnt++;
-                if (cnt > 1) {
+                if(cnt>1){
                     return false;
                 }
-                if (i == 0 || nums[i - 1] <= nums[i + 1]) {
-                    nums[i] = nums[i + 1];
-                } else {
-                    nums[i + 1] = nums[i];
+                if(i == 0 || nums[i-1] <= nums[i+1]){
+                    nums[i] = nums[i+1];
+                }else{
+                    nums[i+1] = nums[i];
                 }
             }
-
         }
         return true;
     }
