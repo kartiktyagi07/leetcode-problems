0d6046f5b319890d75a21b5a68c846b8b06cba05
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-11-14 13:08:09 +0530

    Time: 0 ms (100%), Space: 44.1 MB (13.45%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -1,40 +1,30 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        // Create a dummy node to serve as the new head of the merged list.
-        ListNode dummy = new ListNode(-1);
-        // 'current' pointer to build the new list.
-        ListNode current = dummy;
-
-        // Iterate while both lists have nodes to compare.
-        while (list1 != null && list2 != null) {
-            if (list1.val <= list2.val) {
-                current.next = list1;
-                list1 = list1.next;
-            } else {
-                current.next = list2;
-                list2 = list2.next;
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        ListNode i=list1;
+        ListNode j=list2;
+        while(i != null && j != null){
+            if(i.val <= j.val){
+                curr.next = i;
+                i = i.next;
+            }else{
+                curr.next = j;
+                j = j.next;
             }
-            current = current.next;
+            curr = curr.next;
         }
-
-        // Attach the remaining nodes from the non-empty list.
-        if (list1 != null) {
-            current.next = list1;
-        } else if (list2 != null) {
-            current.next = list2;
-        }
-
-        // The merged list starts from the next node of the dummy.
+        curr.next = i != null ? i : j;
         return dummy.next;
     }
 }
