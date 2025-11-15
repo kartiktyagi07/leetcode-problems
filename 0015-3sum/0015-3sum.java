3b20361f825e9e868b108c07aa99c70bcde1b4ab
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-11-15 19:22:08 +0530

    Time: 34 ms (33.34%), Space: 59.3 MB (6.25%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,22 +1,28 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> ans=new HashSet<>();
+        int n = nums.length;
+        List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(nums);
-        int n=nums.length;
-        for(int i=0;i<n;i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
-            Map<Integer,Integer> map=new HashMap<>();
-            int tar=-nums[i];
-            for(int j=i+1;j<n;j++){
-                int comp=tar-nums[j];
-                if(map.containsKey(comp)){
-                    List<Integer> temp=Arrays.asList(nums[i],comp,nums[j]);
-                    Collections.sort(temp);
-                    ans.add(temp);
+        for(int i=0;i<n-2;i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            int target = -nums[i];
+            int left = i+1;
+            int right = n-1;
+            while(left<right){
+                int sum = nums[left] + nums[right];
+                if(sum == target){
+                    list.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                    while(left<right && nums[left] == nums[left+1]) left++;
+                    while(left<right && nums[right] == nums[right-1]) right--;
+                    left++;
+                    right--;
+                }else if (sum>target){
+                    right--;
+                }else{
+                    left++;
                 }
-                map.put(nums[j],j);
             }
         }
-        return new ArrayList<>(ans);
+        return list;
     }
 }
