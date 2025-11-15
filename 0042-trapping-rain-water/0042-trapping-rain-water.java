2c9c75f246c24f354acbdd3382907e5e64fb4660
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-11-16 00:08:18 +0530

    Time: 1 ms (57.89%), Space: 48.7 MB (5.72%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,33 +1,28 @@
 class Solution {
     public int trap(int[] height) {
-        int ans=0;
-        int n=height.length;
-        // Building an array for leftMax
-        int[] leftMax=new int[n];
-        leftMax[0]=height[0];
-        int left_max=height[0];
+        int ans = 0;
+        int n = height.length;
+        int[] leftMaxArr = new int[n];
+        leftMaxArr[0] = height[0];
+        int leftMax = height[0];
         for(int i=1;i<n;i++){
-            if(height[i]>left_max){
-                left_max=height[i];
+            if(height[i]>leftMax){
+                leftMax = height[i];
             }
-            leftMax[i]=left_max;
+            leftMaxArr[i] = leftMax;
         }
-
-        // Building an array for rightMax
-        int[] rightMax = new int[n];
-        rightMax[n - 1] = height[n - 1];
-        int right_max = height[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            if (height[i] > right_max) {
-                right_max = height[i];
+        int[] rightMaxArr = new int[n];
+        rightMaxArr[n-1] = height[n-1];
+        int rightMax = height[n-1];
+        for(int i=n-2;i>=0;i--){
+            if(height[i]>rightMax){
+                rightMax = height[i];
             }
-            rightMax[i] = right_max;
+            rightMaxArr[i] = rightMax;
         }
-
-        //Traversing the original answer and building the answer
         for(int i=0;i<n;i++){
-            int water=Math.min(leftMax[i],rightMax[i])-height[i];
-            ans+=water;
+            int water = Math.min(leftMaxArr[i],rightMaxArr[i]) - height[i];
+            ans += water;
         }
         return ans;
     }
