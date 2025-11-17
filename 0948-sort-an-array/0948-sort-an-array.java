e9762a4170c5e748715b98f6cb0fc756d6881937
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-11-17 10:32:52 +0530

    Time: 26 ms (65.92%), Space: 86.6 MB (6.33%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,42 +1,42 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-     merge(nums,0,nums.length);
-     return nums;   
+        merge(nums,0,nums.length);
+        return nums;
     }
-    static void merge(int[] arr,int s,int e){
-        if(e-s<=1) return;
-        int mid=(e+s)/2;
-        merge(arr,s,mid);
-        merge(arr,mid,e);
-        mix(arr,s,mid,e);
+    public void merge(int[] nums,int s,int e){
+        if(e-s <= 1) return;
+        int mid = (e+s)/2;
+        merge(nums,s,mid);
+        merge(nums,mid,e);
+        mixArr(nums,s,mid,e);
     }
-    static void mix(int[] arr,int s,int m,int e){
-        int[] ans=new int[e-s];
-        int i=s;
-        int j=m;
-        int k=0;
+    public void mixArr(int[] nums,int s,int m,int e){
+        int[] ans = new int[e-s];
+        int i = s;
+        int j = m;
+        int k = 0;
         while(i<m && j<e){
-            if(arr[i]<=arr[j]){
-                ans[k]=arr[i];
+            if(nums[i] <= nums[j]){
+                ans[k] = nums[i];
                 i++;
             }else{
-                ans[k]=arr[j];
+                ans[k] = nums[j];
                 j++;
             }
             k++;
         }
         while(i<m){
-            ans[k]=arr[i];
+            ans[k] = nums[i];
             i++;
             k++;
         }
         while(j<e){
-            ans[k]=arr[j];
+            ans[k] = nums[j];
             j++;
             k++;
         }
         for(int x=0;x<ans.length;x++){
-            arr[s+x]=ans[x];
+            nums[s+x] = ans[x];
         }
     }
 }
