a7f42e864173e1cc319909147f7c8d4cf3937484
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-11-19 22:35:18 +0530

    Time: 4 ms (27.82%), Space: 46.1 MB (17.14%) - LeetHub

---
+++ 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findFinalValue(int[] nums, int original) {
+        Set<Integer> set = new HashSet<>();
+        for(int x:nums){
+            set.add(x);
+        }
+        while(set.contains(original)){
+            original = 2 * original;
+        }
+        return original;
+    }
+}
