39dafbd0b696d5d2bed9e3b1ad0f4641b32de056
Kartik Tyagi <kartiktyagiji07@gmail.com>
2025-11-27 16:06:12 +0530

    Time: 6 ms (29.65%), Space: 46.6 MB (5%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,45 +1,43 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> list=new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(candidates);
-        int n=candidates.length;
-        helper(candidates,target,0,list,new ArrayList<>());
-        return list;
+        helper(candidates,target,0,ans,new ArrayList<>());
+        return ans;
     }
-    // General pattern
-    public void helper(int[] arr,int target,int idx,List<List<Integer>> ans,List<Integer> temp){
-        if(target==0){
+    void helper(int[] arr,int target,int idx,List<List<Integer>> ans,List<Integer> temp){
+        if(target == 0){
             ans.add(new ArrayList<>(temp));
             return;
         }
-        if(target<0) return;
-        for(int i=idx;i<arr.length;i++){
-            if(i>idx && arr[i]==arr[i-1]) continue;
+        for(int i = idx ; i < arr.length;i++){
+            if(i > idx && arr[i] == arr[i-1]){
+                continue;
+            }
+            if(arr[i] > target) break;
             temp.add(arr[i]);
             helper(arr,target-arr[i],i+1,ans,temp);
             temp.remove(temp.size()-1);
         }
     }
-
-
-    // Method -> pick and not pick
-
-    // public void helper(int[] arr,int target,int idx,List<List<Integer>> list,int n,List<Integer> temp){
-    //     if(idx==n){
-    //         if(target==0){
-    //             list.add(new ArrayList<>(temp));
+    // Pick and not pick 
+    // void helper(int[] arr,int target,int idx,List<List<Integer>> ans,List<Integer> temp){
+    //     if(idx == arr.length){
+    //         if(target == 0){
+    //             ans.add(new ArrayList<>(temp));
     //         }
     //         return;
     //     }
-    //     if(arr[idx]<=target){
+    //     // Pick
+    //     if(arr[idx] <= target){
     //         temp.add(arr[idx]);
-    //         helper(arr,target-arr[idx],idx+1,list,n,temp);
+    //         helper(arr,target-arr[idx],idx+1,ans,temp);
     //         temp.remove(temp.size()-1);
     //     }
-    //     int nextIdx = idx + 1;
-    //     while (nextIdx < n && arr[nextIdx] == arr[idx]) {
-    //         nextIdx++;
+    //     // Not pick
+    //     while(idx+1 < arr.length && arr[idx+1] == arr[idx]){
+    //         idx++;
     //     }
-    //     helper(arr,target,nextIdx,list,n,temp);
+    //     helper(arr,target,idx+1,ans,temp);
     // }
 }
