eeab33c3b9a3066928ce66fbbcdf0872fd970b20
Kartik Tyagi <kartiktyagiji07@gmail.com>
2026-01-02 12:01:16 +0530

    Time: 2 ms (89.17%), Space: 48 MB (17.89%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,13 +1,25 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int l = 0;
-        for(int r=0;r<nums.length;r++){
-            if(nums[r] != 0){
-                int temp = nums[l];
-                nums[l] = nums[r];
-                nums[r] = temp;
-                l++;
+        // Method 1
+        int idx = 0;
+        for(int x:nums){
+            if(x != 0){
+                nums[idx++] = x;
             }
         }
+        while(idx < nums.length){
+            nums[idx++] = 0;
+        }
+
+        // Method 2
+        // int l = 0;
+        // for(int r=0;r<nums.length;r++){
+        //     if(nums[r] != 0){
+        //         int temp = nums[l];
+        //         nums[l] = nums[r];
+        //         nums[r] = temp;
+        //         l++;
+        //     }
+        // }
     }
 }
