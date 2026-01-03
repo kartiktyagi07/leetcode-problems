2cfbc0bf8d86524ad2badaa848ed30a30af53671
Kartik Tyagi <kartiktyagiji07@gmail.com>
2026-01-03 13:23:27 +0530

    Time: 1554 ms (8.25%), Space: 48.8 MB (47.16%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,30 +1,16 @@
 class Solution {
-    public int subarraySum(int[] nums, int k) {
-        // Brute Force: Got Accepted
-        // int cnt = 0;
-        // int n = nums.length;
-        // for(int i=0;i<n;i++){
-        //     int sum = 0;
-        //     for(int j=i;j<n;j++){
-        //         sum += nums[j];
-        //         if(sum==k){
-        //             cnt++;
-        //         }
-        //     }
-        // }
-        // return cnt;
-
-        // Optimized Solution: HashMap amd prefixSum
-        int prefixSum = 0;
+    public int subarraySum(int[] arr, int k) {
+        // Brute Force
         int cnt = 0;
-        Map<Integer,Integer> map = new HashMap<>();
-        map.put(0,1);
-        for(int num : nums){
-            prefixSum += num;
-            if(map.containsKey(prefixSum-k)){
-                cnt+=map.get(prefixSum-k);
+        int n = arr.length;
+        for(int i=0;i<n;i++){
+            int sum = 0;
+            for(int j=i;j<n;j++){
+                sum += arr[j];
+                if(sum == k){
+                    cnt++;
+                }
             }
-            map.put(prefixSum,map.getOrDefault(prefixSum,0)+1);
         }
         return cnt;
     }
