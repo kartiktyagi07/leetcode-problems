6006e1f4731a95d52cca8a90a0f1867e18f11983
Kartik Tyagi <kartiktyagiji07@gmail.com>
2026-01-05 12:52:29 +0530

    Time: 1 ms (99.74%), Space: 77.4 MB (31.24%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,17 +1,33 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int maxSum = Integer.MIN_VALUE;
+        // Kadane's Algo
+        int ans = Integer.MIN_VALUE;
         int currSum = 0;
-        // if(nums.length == 1){
-        //     return nums[0];
-        // }
         for(int i=0;i<nums.length;i++){
             currSum += nums[i];
-            maxSum = Math.max(currSum,maxSum);
-            if(currSum<=0){
+            ans = Math.max(ans,currSum);
+            if(currSum <= 0){
                 currSum = 0;
             }
         }
-        return maxSum;
+        return ans;
+
+
+
+
+
+        // // Brute Force gives TLE
+        // int max = Integer.MIN_VALUE;
+        // int n = nums.length;
+        // for (int i = 0; i < n; i++) {
+        //     int currSum = 0;
+        //     for (int j = i; j < n; j++) {
+        //         currSum += nums[j];
+        //         if (currSum >= max) {
+        //             max = currSum;
+        //         }
+        //     }
+        // }
+        // return max;
     }
 }
