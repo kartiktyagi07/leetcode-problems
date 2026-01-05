cdbaa66e924f788a721286100b36f6bfb1406ada
Kartik Tyagi <kartiktyagiji07@gmail.com>
2026-01-05 12:04:32 +0530

    Time: 0 ms (100%), Space: 43.6 MB (23.81%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,47 +1,22 @@
 class Solution {
-    public void sortColors(int[] arr) {
-        // int cnt0=0;
-        // int cnt1=0;
-        // int cnt2=0;
-        // for(int i=0;i<arr.length;i++){
-        //     if(arr[i]==0){
-        //         cnt0++;
-        //     }else if(arr[i]==1){
-        //         cnt1++;
-        //     }else{
-        //         cnt2++;
-        //     }
-        // }
-        // for(int idx=0;idx<cnt0;idx++){
-        //     arr[idx]=0;
-        // }
-        // for(int idx=cnt0;idx<cnt0+cnt1;idx++){
-        //     arr[idx]=1;
-        // }
-        // for(int idx=cnt0+cnt1;idx<cnt0+cnt1+cnt2;idx++){
-        //     arr[idx]=2;
-        // }
-
-
-        int s=0;
-        int e=arr.length-1;
-        int m=0;
-        while(m<=e){
-            if(arr[m]==0){
-                swap(arr,s,m);
+    public void sortColors(int[] nums) {
+        int s = 0,m = 0,e = nums.length-1;
+        while(m <= e){
+            if(nums[m] == 0){
+                swap(nums,s,m);
                 s++;
                 m++;
-            }else if(arr[m]==1){
+            }else if(nums[m] == 1){
                 m++;
             }else{
-                swap(arr,m,e);
+                swap(nums,m,e);
                 e--;
             }
         }
     }
-    public static void swap(int[] arr,int low,int high){
-        int temp=arr[low];
-        arr[low]=arr[high];
-        arr[high]=temp;
+    public void swap(int[] nums,int s,int e){
+        int temp = nums[s];
+        nums[s] = nums[e];
+        nums[e] = temp;
     }
 }
