b2083b5aa05a8354b2d1a6dc0d71023869971837
Kartik Tyagi <kartiktyagiji07@gmail.com>
2026-01-05 12:42:34 +0530

    Time: 1 ms (99.82%), Space: 56 MB (7.01%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,37 +1,16 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        // -------->BRUTE FORCE<--------
-
-        // int max=(int)Math.floor(nums.length/2);
-        // int ans=0;
-        // for(int i=0;i<nums.length;i++){
-        //     int count=0;
-        //     int tar=nums[i];
-        //     for(int j=0;j<nums.length;j++){
-        //         if(nums[j]==tar){
-        //             count++;
-        //         }
-        //     }
-        //     if(count>max){
-        //         ans=tar;
-        //         return ans; 
-        //     }
-        // }
-        // return ans;
-
-
-        // ------->OPTIMIZED APPROACH<-------
-        // MOORE VOTING ALGORITHM
-        int ans=0;
-        int count=0;
+        // Moore voting algo
+        int cnt = 0;
+        int ans = 0;
         for(int i=0;i<nums.length;i++){
-            if(count==0){
-                ans=nums[i];
+            if(cnt == 0){
+                ans = nums[i];
             }
-            if(nums[i]==ans){
-                count++;
+            if(nums[i] == ans){
+                cnt++;
             }else{
-                count--;
+                cnt--;
             }
         }
         return ans;
