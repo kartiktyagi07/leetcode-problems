0dbfe18bcc2ab950ce2ca4f45d2f3b59a05d12e3
Kartik Tyagi <kartiktyagiji07@gmail.com>
2026-01-06 13:43:37 +0530

    Time: 1 ms (99.76%), Space: 94.5 MB (50.58%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,28 +1,12 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        // Brute Force using nested loops
-
-        // int ans=0;
-        // int n=prices.length;
-        // for(int i=0;i<n;i++){
-        //     for(int j=i+1;j<n;j++){
-        //         int profit=prices[j]-prices[i];
-        //         ans=Math.max(ans,profit);
-        //     }
-        // }
-        // return ans;
-
-
-
-        // Optimized Approach
-        int ans=0;
-        int n=prices.length;
-        int min=Integer.MAX_VALUE;
+        int ans = 0;
+        int n = prices.length;
+        int minSoFar = Integer.MAX_VALUE;
         for(int i=0;i<n;i++){
-            min=Math.min(min,prices[i]);
-            ans=Math.max(ans,prices[i]-min);
+            minSoFar = Math.min(minSoFar,prices[i]);
+            ans = Math.max(ans,prices[i] - minSoFar);
         }
         return ans;
-
     }
 }
