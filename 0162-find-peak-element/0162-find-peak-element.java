a602fe6844699a873def3ad0c77a107708819bdb
Kartik Tyagi <kartiktyagiji07@gmail.com>
2026-01-15 23:04:18 +0530

    Time: 0 ms (100%), Space: 44.3 MB (39.73%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,12 +1,28 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        // Brute Force
+        // Optimized Approach
         int n = nums.length;
-        for(int i=0;i<n-1;i++){
-            if(nums[i]>nums[i+1]){
-                return i;
+        int s = 0;
+        int e = n - 1;
+        while(s < e){
+            int mid = s + (e - s) / 2;
+            if(nums[mid]>nums[mid+1]){
+                e = mid;
+            }else{
+                s = mid + 1;
             }
         }
-        return n-1;
+        return s;
+
+
+
+        // // Brute Force
+        // int n = nums.length;
+        // for(int i=0;i<n-1;i++){
+        //     if(nums[i]>nums[i+1]){
+        //         return i;
+        //     }
+        // }
+        // return n-1;
     }
 }
