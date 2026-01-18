0edf22bc43cf92769ab6d5db013647f81a0b9120
Kartik Tyagi <kartiktyagiji07@gmail.com>
2026-01-18 21:05:52 +0530

    Time: 1 ms (96.47%), Space: 42.5 MB (83.51%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,8 +1,14 @@
 class Solution {
     public int mySqrt(int x) {
-        int z =  (int) Math.pow(x,0.5); 
-        // int y= (int) z;  
-        return z;
+        if(x == 0 || x == 1) return x;
+        int s = 0;
+        int e = x;
+        while(s <= e){
+            int mid = s + (e - s) / 2;
+            if(mid*mid == x) return mid;
+            else if((long)mid*mid > (long)x) e = mid - 1;
+            else s = mid + 1;
+        }
+        return Math.round(e);
     }
-    
 }
